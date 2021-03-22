6ca3d43b092cba30960834324446724786d75d8e
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-03-22 17:54:47 +0100

    BONUS: Cambio immagine con dots (non sincronizzato con frecce)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Funzione immagine successiva
 function nextImg(){
     var activeImg = $('.active');
     activeImg.removeClass('active');
@@ -8,11 +9,13 @@ function nextImg(){
         var nextImg = activeImg.next('img');
         activeImg = nextImg;
     }
-    
+
     activeImg.addClass('active');
+
     console.log('Immagine attiva: ' + activeImg[0].alt);
 }
 
+// Funzione immagine precedente
 function prevImg(){
     var activeImg = $('.active');
     activeImg.removeClass('active');
@@ -28,12 +31,32 @@ function prevImg(){
     console.log('Immagine attiva: ' + activeImg[0].alt);
 }
 
+// Funzione cambio immagine via Dots
+function clickedDot() {
+    var dotNumber = $(this).attr('data-imgPos');
 
+    var activeImg = $('img.active');
+    activeImg.removeClass('active');
+
+    activeImg = $('img').eq(dotNumber);
+    activeImg.addClass('active');
+
+    var prevDot = $('i.fas');
+    prevDot.removeClass('fas');
+    prevDot.addClass('far');
+
+    var thisDot = $(this);
+    thisDot.removeClass('far');
+    thisDot.addClass('fas');
+}
 
 function init(){
     // Click avanti
     $('#right-arrow').click(nextImg);
     $('#left-arrow').click(prevImg);
+
+    // Click dots
+    $('.dots i').click(clickedDot)
 }
 
 $(document).ready(init);
