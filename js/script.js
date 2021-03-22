7112de9b454cca2fcc7df087acbd5e9619c3a343
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-03-22 15:53:57 +0100

    Esercizio completo: funzione prevImg completa

--- js/script.js
+++ js/script.js
@@ -10,12 +10,30 @@ function nextImg(){
     }
     
     activeImg.addClass('active');
+    console.log('Immagine attiva: ' + activeImg[0].alt);
 }
+
+function prevImg(){
+    var activeImg = $('.active');
+    activeImg.removeClass('active');
+
+    if(activeImg.hasClass('firstImg')){
+        activeImg = $('.lastImg');
+    }else{
+        var nextImg = activeImg.prev('img');
+        activeImg = nextImg;
+    }
+    
+    activeImg.addClass('active');
+    console.log('Immagine attiva: ' + activeImg[0].alt);
+}
+
 
 
 function init(){
     // Click avanti
     $('#right-arrow').click(nextImg);
+    $('#left-arrow').click(prevImg);
 }
 
 $(document).ready(init);
